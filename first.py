990d4d62cb199fef058a7b1e65cbfa7094b0efcf
sonamsingh10 <sonamkumari29019@gmail.com>
2025-02-25 23:04:30 +0530

    Add new paragraph

---
+++ first.py
@@ -0,0 +1,18 @@
+a=5
+b=6
+print(a+b)
+
+a=9
+b=7
+print(a-b)
+
+a=7
+b=8
+print(a*b)
+
+import numpy as np
+one_darray=np.array([2,5,7,8])
+print(one_darray)
+import numpy as np
+arr2d=np.array([[2,7,9,8],[9,8,7,6]])
+print(arr2d)
